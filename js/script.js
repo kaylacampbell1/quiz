0bb14ca91427bec2a923fa93176a5f07052865e4
Kayla Campbell <campbellkayla2003@gmail.com>
2018-12-19 22:54:13 +0000

    return statements

--- js/script.js
+++ js/script.js
@@ -8,7 +8,47 @@ $(document).ready(function() {
         var q1Result = $("#question1").val();
         var q2Result = $("#question2").val();
         var q3Result = $("#question3").val();
-        var totalScore;
+        var totalScore = 0;
+        q1Result.toLowerCase();
+        q2Result.toLowerCase();
+        q3Result.toLowerCase();
+        question1(q1Result, totalScore);
+        question2(q2Result, totalScore);
+        question3(q3Result, totalScore);
+        
     });
 
 });
+
+function question1(a1, score){
+    if (a1==="walking away"){
+        
+        return score+=3
+    } if (a1==="cheering"){
+        
+        return score+=9
+    } if (a1==="stopping it"){
+        
+        return score+=1
+    }
+}
+function question2(a2){
+    if (a2==="walk"){
+        
+        return
+    } if (a2==="fly"){
+        
+    } if (a2==="swim"){
+        
+    }
+    
+}
+function question3(a3){
+    if (a3==="omnivore"){
+        
+    } if (a3==="carnivore"){
+        
+    } if (a3==="herbivore"){
+        
+    }
+}
