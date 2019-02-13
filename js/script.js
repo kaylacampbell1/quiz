b95c152c9082ee11d90d3b4b23e7f6eb44b1ef16
Kayla Campbell <campbellkayla2003@gmail.com>
2019-02-13 21:21:13 +0000

    complete

--- js/script.js
+++ js/script.js
@@ -12,43 +12,67 @@ $(document).ready(function() {
         q1Result.toLowerCase();
         q2Result.toLowerCase();
         q3Result.toLowerCase();
-        question1(q1Result, totalScore);
-        question2(q2Result, totalScore);
-        question3(q3Result, totalScore);
+        totalScore += question1(q1Result);
+        totalScore += question2(q2Result);
+        totalScore += question3(q3Result);
         
+        total(totalScore, name, q2Result);
     });
 
 });
 
-function question1(a1, score){
-    if (a1==="walking away"){
-        
-        return score+=3
+function question1(a1){
+    if (a1==="moving away"){
+        return 3
     } if (a1==="cheering"){
         
-        return score+=9
+        return 9
     } if (a1==="stopping it"){
         
-        return score+=1
+        return 1
     }
 }
+
 function question2(a2){
     if (a2==="walk"){
         
-        return
+        return 6
     } if (a2==="fly"){
         
+        return 1
     } if (a2==="swim"){
         
+        return 3
     }
     
 }
 function question3(a3){
     if (a3==="omnivore"){
         
+        return 5
     } if (a3==="carnivore"){
         
+        return 12
     } if (a3==="herbivore"){
         
+        return 3
     }
+
+    
+} 
+function total(score, name,q2){
+    if (score<=6){
+            $(".result").html(name + " your spirit animal is a Butterfly");
+            $(".img").append("<img src=\"https://images-na.ssl-images-amazon.com/images/I/71H-XvaGZwL.png\">");
+        }else if(score>6 && score<=20){
+            $(".result").html(name + " your spirit animal is a Lion");
+            $(".img").append("<img src=\"http://www.stickpng.com/assets/images/580b57fbd9996e24bc43bc89.png\">");
+        }if(q2==="swim"){
+            $(".result").html(name + " your spirit animal is a Dolphin");
+            $(".img").append("<img src=\"https://cdn.iflscience.com/images/aed9b8cd-e206-57ee-bc3f-5ec1c52e3bb5/default-1535474393-cover-image.jpg\">");
+        }else if(score>20){
+            $(".result").html(name + " your spirit animal is an Alligator");
+            $(".img").append("<img src=\"https://cdn3.volusion.com/kapts.nrbqf/v/vspfiles/photos/ALLIGATOR114-2.jpg?1496409148\">");
+
+        }
 }
